5c6224599f8a6c9a178af5b7a3a77885b62fb96f
misha1391 <misha.martynov2009.1@gmail.com>
2026-03-16 14:59:45 +0300

    Finish

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ran = new Random(); // В отличии от c++ и python, seed определяется автоматически, поэтому необязательно добавлять таймер сюда
+            int number = ran.Next()%100;
+            Console.WriteLine("Загадано число от 0 до 100, введите это число:");
+            int attempts = 0;
+            while (true)
+            {
+                int guess = int.Parse(Console.ReadLine());
+                attempts++;
+                if (number < guess) Console.WriteLine("Заданное число меньше вашего");
+                else if (number > guess) Console.WriteLine("Заданное число больше вашего");
+                else
+                {
+                    Console.WriteLine($"Вам получилось угадать число за {attempts} попыток");
+                    break;
+                }    
+            }
+        }
+    }
+}
